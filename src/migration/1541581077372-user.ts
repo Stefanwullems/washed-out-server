0e34fad9d458f6a2662c16b57019a7b7b3759d85
Stefanwullems <stefanwullems1998@gmail.com>
2018-11-07 11:11:53 +0100

    added dummy data for user and location

---
+++ src/migration/1541581077372-user.ts
@@ -0,0 +1,60 @@
+import { MigrationInterface, QueryRunner, createQueryBuilder } from "typeorm";
+import User from "../entity/User";
+import Location from "../entity/Location";
+import getGeolocation, {
+  ISearchGeolocationParams
+} from "../scripts/geolocation";
+import * as bcrypt from "bcryptjs";
+
+export class user1541581077372 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {
+    await createQueryBuilder()
+      .insert()
+      .into(User)
+      .values(
+        await Promise.all([
+          createUser({
+            fullName: "Stefan Wullems",
+            email: "stefanwullems1998@gmail.com",
+            password: "password",
+            bio: "Hi, I'm Stefan",
+            picture:
+              "https://lh3.googleusercontent.com/-ujrTwbBejaA/UrnMCmdEZyE/AAAAAAAAAHU/JuXmkxujrTYBHz6-AXaUzNF7PbWORiL1QCEwYCg/w163-h110-p/ProfilePhotos02",
+            locationInput: {
+              street: "rijksweg",
+              houseNumber: "342",
+              postalCode: "2071 CR",
+              city: "santpoort-noord",
+              country: "the netherlands"
+            }
+          })
+        ])
+      );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<any> {}
+}
+
+interface IUserParams {
+  fullName: string;
+  email: string;
+  password: string;
+  bio: string;
+  picture: string;
+  locationInput: ISearchGeolocationParams;
+}
+
+async function createUser({
+  password,
+  locationInput,
+  ...userInput
+}: IUserParams) {
+  const location = await Location.create(
+    await getGeolocation(locationInput)
+  ).save();
+  return User.create({
+    ...userInput,
+    location,
+    password: await bcrypt.hash(password, 10)
+  }).save();
+}
--- src/scripts/geolocation.ts
+++ src/scripts/geolocation.ts
@@ -1,21 +1,48 @@
 import * as request from "superagent";
+import Location from "../entity/Location";
 
 //TODO remove hardcoded key
 const appId = "YKXp7WGeg0GYfDvclA2i"; //process.env.APP_ID
 const appCode = "c7bC2pg6dcKRYGbequ9fSA"; //process.env.APP_CODE
 
-interface ISearchGeolocationParams {
+export interface ISearchGeolocationParams {
   street: string;
+  houseNumber: string;
+  addition?: string;
   postalCode: string;
   city: string;
   country: string;
 }
 
-export default function getGeolocation({ street, postalCode, city, country }) {
-  request
+export default async function getGeolocation({
+  street,
+  postalCode,
+  city,
+  country,
+  houseNumber,
+  addition
+}: ISearchGeolocationParams): Promise<Partial<Location>> {
+  const address = addition
+    ? street + " " + houseNumber + addition
+    : street + " " + houseNumber;
+  const res = await request
     .get(
-      `https://geocoder.api.here.com/6.2/geocode.json?app_id=${appId}&app_code=${appCode}&searchtext=${street}+${postalCode}+${city}+${country}`
+      `https://geocoder.api.here.com/6.2/geocode.json?app_id=${appId}&app_code=${appCode}&searchtext=${address}+${postalCode}+${city}+${country}`
     )
-    .then(res => console.log(res.body.Response.View[0].Result[0].Location))
+    .then(res => res.body.Response.View[0].Result[0].Location)
     .catch(console.error);
+  return {
+    locationId: res.LocationId as string,
+    streetName: res.Address.Street as string,
+    houseNumber: res.Address.HouseNumber as string,
+    postalCode: res.Address.PostalCode as string,
+    city: res.Address.City as string,
+    country: res.Address.Country as string,
+    latitude:
+      (res.NavigationPosition[0].Latitude as number) ||
+      (res.DisplayPosition.Latitude as number),
+    longitude:
+      (res.NavigationPosition[0].Longitude as number) ||
+      (res.DisplayPosition.Longitude as number)
+  };
 }
