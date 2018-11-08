1b19a02e381cfde3ffe781ab3da1038e6251a1b8
yugaWicaksono <wicaksono.yuga@gmail.com>
2018-11-08 10:54:27 +0100

    changed the auth-controller

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,4 +1,4 @@
 import UserController from "./UserController";
 import ServicesController from "./ServicesController";
-import  AuthController  from "./AuthController";
+import {AuthController}  from "./AuthController";
 export default [UserController, AuthController, ServicesController];
