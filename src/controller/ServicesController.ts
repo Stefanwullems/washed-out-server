b88d1aa18d17e8ce4242badfa2dba12fe9725830
Stefanwullems <stefanwullems1998@gmail.com>
2018-11-12 10:22:37 +0100

    finally figured out services thingy

--- src/controller/ServicesController.ts
+++ src/controller/ServicesController.ts
@@ -1,18 +1,13 @@
 import { Controller, Mutation } from "vesper";
 import Services from "../entity/Services";
-import User from "../entity/User";
-import { createQueryBuilder } from "typeorm";
 
 @Controller()
 export default class ServicesController {
   @Mutation()
   async updateServices(args) {
-    const { userId, ...servicesUpdate } = args;
+    const { id, ...servicesUpdate } = args;
 
-    const user = await User.findOne(userId);
-    if (!user) throw new Error("User does not exist");
-
-    const prevServices = await Services.findOne();
+    const prevServices = await Services.findOne(id);
 
     const services = await Services.merge(prevServices, servicesUpdate).save();
 
